096865fddbc6d3a60247e1a2a6748d2625ccd922
Drew <tullos.andrew@gmail.com>
2025-01-12 10:31:05 -0600

    Class init

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class NumberGuessingGame {
     public static void main(String[] args) {
+    NumberGuessingGame game = new NumberGuessingGame();
+    game.start();
+    }
 
+    private void start() {
+        Scanner console = new Scanner(System.in);
+        Random random = new Random();
+
+
+        System.out.println("Welcome to the Number Guessing Game!");
     }
 }
